db8192daf55aab0824f614596414c880fa9c530f
jwvonk <jwavonk@gmail.com>
2024-02-03 20:30:16 -0800

    Big

--- src/asg5_will.js
+++ src/asg5_will.js
@@ -10,7 +10,8 @@ function main() {
   renderer.xr.enabled = true;
   document.body.appendChild(VRButton.createButton(renderer));
 
-  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.enabled = false;
+  renderer.shadowMap.type = THREE.BasicShadowMap;
   renderer.outputColorSpace = THREE.SRGBColorSpace;
 
   const base = new THREE.Object3D();
@@ -20,16 +21,16 @@ function main() {
   const fov = 75;
   const aspect = canvas.clientWidth / canvas.clientHeight;
   const near = 0.1;
-  const far = 50;
+  const far = 500;
 
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   const cameraHelper = new THREE.CameraHelper(camera);
   helpers.push(cameraHelper);
 
-  const cameraGroup = new THREE.Object3D();
-  cameraGroup.position.set(1, 1, -1);
-  cameraGroup.add(camera)
-  base.add(cameraGroup);
+  const cameraParent = new THREE.Object3D();
+  cameraParent.position.set(-7.5, .7, 7.5);
+  cameraParent.add(camera);
+  base.add(cameraParent);
 
   const scene = new THREE.Scene();
   {
@@ -47,34 +48,35 @@ function main() {
   {
     const color = 0xd9c896;
     const near = 5;
-    const far = 45;
+    const far = 450;
     scene.fog = new THREE.Fog(color, near, far);
   }
 
   {
-    const planeSize = 100;
+    const planeSize = 1000;
     const loader = new THREE.TextureLoader();
     const disp = loader.load("../assets/Will/aerial_beach_01_disp_2k.jpg");
     disp.wrapS = THREE.RepeatWrapping;
     disp.wrapT = THREE.RepeatWrapping;
     disp.magFilter = THREE.NearestFilter;
-    disp.repeat.set(planeSize / 32, planeSize / 32);
+    disp.repeat.set(planeSize / 320, planeSize / 320);
 
     const norm = loader.load("../assets/Will/aerial_beach_01_nor_gl_2k.jpg");
     norm.wrapS = THREE.RepeatWrapping;
     norm.wrapT = THREE.RepeatWrapping;
     norm.magFilter = THREE.NearestFilter;
-    norm.repeat.set(planeSize / 32, planeSize / 32);
+    norm.repeat.set(planeSize / 320, planeSize / 320);
 
-    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize, 500, 500);
+    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize, 4000, 4000);
     const planeMat = new THREE.MeshPhongMaterial({
       color: 0xd9c896,
       displacementMap: disp,
-      displacementScale: 0.7,
+      displacementScale: 7,
       normalMap: norm,
       side: THREE.DoubleSide,
     });
     const mesh = new THREE.Mesh(planeGeo, planeMat);
+    mesh.position.set(0, -3, 0);
     mesh.receiveShadow = true;
     mesh.rotation.x = Math.PI * -0.5;
     scene.add(mesh);
@@ -92,13 +94,13 @@ function main() {
     const intensity = 0.9;
     const light = new THREE.DirectionalLight(color, intensity);
     light.castShadow = true;
-    light.position.set(50, 15, -50);
+    light.position.set(500, 150, -500);
     light.target.position.set(0, 0, 0);
     light.shadow.camera.zoom = 1;
-    light.shadow.camera.left *= 5;
-    light.shadow.camera.right *= 5;
-    light.shadow.camera.top *= 2;
-    light.shadow.camera.bottom *= 1.2;
+    light.shadow.camera.left *= 50;
+    light.shadow.camera.right *= 50;
+    light.shadow.camera.top *= 20;
+    light.shadow.camera.bottom *= 12;
     light.shadow.mapSize.width = 2048;
     light.shadow.mapSize.height = 2048;
     scene.add(light);
@@ -114,13 +116,13 @@ function main() {
     const pyramidMat = new THREE.MeshPhongMaterial({ color: "#CA8" });
     let y = 0.25;
     for (let x = 15; x > 0; x--) {
-      const pyramidGeo = new THREE.BoxGeometry(x, 0.5, x);
+      const pyramidGeo = new THREE.BoxGeometry(10 * x, 5, 10 * x);
       const mesh = new THREE.Mesh(pyramidGeo, pyramidMat);
       mesh.receiveShadow = true;
       mesh.castShadow = true;
-      mesh.position.set(9, y, 8);
+      mesh.position.set(90, y, 80);
       scene.add(mesh);
-      y += 0.5;
+      y += 5;
     }
   }
 
@@ -128,13 +130,13 @@ function main() {
     const pyramidMat = new THREE.MeshPhongMaterial({ color: "#CA8" });
     let y = 0.25;
     for (let x = 15; x > 0; x--) {
-      const pyramidGeo = new THREE.BoxGeometry(x, 0.5, x);
+      const pyramidGeo = new THREE.BoxGeometry(10 * x, 5, 10 * x);
       const mesh = new THREE.Mesh(pyramidGeo, pyramidMat);
       mesh.receiveShadow = true;
       mesh.castShadow = true;
-      mesh.position.set(-8, y, -9);
+      mesh.position.set(-80, y, -90);
       scene.add(mesh);
-      y += 0.5;
+      y += 5;
     }
   }
 
@@ -148,7 +150,8 @@ function main() {
     });
 
     const jewel = new THREE.Mesh(jewelGeo, jewelMat);
-    jewel.position.set(9, 9, 8);
+    jewel.position.set(90, 0, 80);
+    jewel.scale.set(10, 10, 10);
     jewel.castShadow = true;
     scene.add(jewel);
     jewels.push(jewel);
@@ -163,7 +166,8 @@ function main() {
     });
 
     const jewel = new THREE.Mesh(jewelGeo, jewelMat);
-    jewel.position.set(-8, 9, -9);
+    jewel.position.set(-80, 0, -90);
+    jewel.scale.set(10, 10, 10);
     jewel.castShadow = true;
     scene.add(jewel);
     jewels.push(jewel);
@@ -174,12 +178,11 @@ function main() {
     const color = 0x289964;
     const intensity = 1;
     const light = new THREE.SpotLight(color, intensity);
-    light.position.set(9, 9, 8);
-    light.target.position.set(0, 1, 0);
+    light.position.set(90, 0, 80);
     light.penumbra = 0.1;
     light.angle = (10 * Math.PI) / 180;
     light.castShadow = true;
-    light.shadow.camera.far = 40;
+    light.shadow.camera.far = 20;
     scene.add(light);
     base.add(light.target);
     spotLights.push(light);
@@ -195,12 +198,11 @@ function main() {
     const color = 0x997d28;
     const intensity = 1;
     const light = new THREE.SpotLight(color, intensity);
-    light.position.set(-8, 9, -9);
-    light.target.position.set(0, 1, 0);
+    light.position.set(-80, 0, -90);
     light.penumbra = 0.1;
     light.angle = (10 * Math.PI) / 180;
     light.castShadow = true;
-    light.shadow.camera.far = 40;
+    light.shadow.camera.far = 20;
     scene.add(light);
     base.add(light.target);
     spotLights.push(light);
@@ -221,8 +223,7 @@ function main() {
       const objLoader = new OBJLoader();
       objLoader.setMaterials(mtl);
       objLoader.load("../assets/Will/pirateship/model.obj", (root) => {
-        root.scale.set(4, 4, 4);
-        root.position.set(0, 1, 0);
+        root.scale.set(40, 40, 40);
         root.rotation.y = (90 * Math.PI) / 180;
         base.add(root);
         root.traverse((child) => {
@@ -243,19 +244,10 @@ function main() {
       camera.updateProjectionMatrix();
     }
 
-    // Debug
-    let visible = false;
-    helpers.forEach((helper) => {
-      if (helper.visible != visible) {
-        helper.visible = visible;
-        helper.update();
-      }
-    });
-
     jewels.forEach((jewel, ndx) => {
       const speed = 1 + ndx * 0.1;
       const rot = time * speed;
-      const ele = (Math.sin(time + ndx) * speed) / 2 + 9;
+      const ele = Math.sin(time + ndx) * speed * 5 + 90;
       jewel.rotation.x = rot;
       jewel.rotation.y = rot;
       jewel.position.y = ele;
@@ -263,13 +255,22 @@ function main() {
 
     spotLights.forEach((light, ndx) => {
       const speed = 1 + ndx * 0.1;
-      const ele = (Math.sin(time + ndx) * speed) / 2 + 10.5;
+      const ele = Math.sin(time + ndx) * speed * 5 + 90;
       light.position.y = ele;
     });
 
     base.position.x = (time % 40) - 20;
     base.position.z = -((time % 40) - 20);
-    base.position.y = Math.sin(time) * 0.05 + 0.1;
+    base.position.y = Math.sin(time) * 0.5 + 10;
+
+    // Debug
+    let visible = true;
+    helpers.forEach((helper) => {
+      if (helper.visible != visible) {
+        helper.visible = visible;
+        helper.update();
+      }
+    });
 
     renderer.render(scene, camera);
   }
